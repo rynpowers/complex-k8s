01670c92c9c07c7e8a364679811e8fef54dd173a
rynpowers <rynpowers@gmail.com>
2023-09-18 17:15:58 -0400

    fix image update

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ docker push rynpowers/complex-server:$SHA
 docker push rynpowers/complex-worker:latest
 docker push rynpowers/complex-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment server=rynpowers/complex-client:$SHA
+kubectl set image deployments/client-deployment client=rynpowers/complex-client:$SHA
 kubectl set image deployments/server-deployment server=rynpowers/complex-server:$SHA
-kubectl set image deployments/worker-deployment server=rynpowers/complex-worker:$SHA
+kubectl set image deployments/worker-deployment worker=rynpowers/complex-worker:$SHA
