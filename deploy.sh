ec9620d201ee9ea97dd8f459f0abbd42b821d5df
rynpowers <rynpowers@gmail.com>
2023-09-18 16:32:01 -0400

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t rynpowers/complex-client:latest -t rynpowers/complex-client:$SHA  -f ./client/Dockerfile
+docker build -t rynpowers/complex-server:latest -t rynpowers/complex-server:$SHA  -f ./server/Dockerfile
+docker build -t rynpowers/complex-worker:latest -t rynpowers/complex-worker:$SHA  -f ./worker/Dockerfile
+docker push rynpowers/complex-client:latest
+docker push rynpowers/complex-client:$SHA
+docker push rynpowers/complex-server:latest
+docker push rynpowers/complex-server:$SHA
+docker push rynpowers/complex-worker:latest
+docker push rynpowers/complex-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment server=rynpowers/complex-client:$SHA
+kubectl set image deployments/server-deployment server=rynpowers/complex-server:$SHA
+kubectl set image deployments/worker-deployment server=rynpowers/complex-worker:$SHA
