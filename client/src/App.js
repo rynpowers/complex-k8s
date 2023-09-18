d13437832484f00a2ba51ec0e3c9265a4cf0b189
rynpowers <rynpowers@gmail.com>
2023-09-18 18:07:00 -0400

    test workflow

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Potato-1
+            Potato-K8s!!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
